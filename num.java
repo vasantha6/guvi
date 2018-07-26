7cfe6f1757fba318e946866ee7ee011da69af160
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-07-26 15:35:14 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,24 @@
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner sc=new Scanner(System.in);
+		 int num=sc.nextInt();
+		 if(num>0)
+		 {
+		 	System.out.println("number is +ve");
+		 }
+		 if(num<0)
+		 {
+		 	System.out.println("number is -ve");
+		 }
+		 if(num==0)
+		 {
+		    System.out.println("number is neither +ve nor -ve");	
+		 }
+	}
+}
