80764211913093a938a8dd03c7e30dad4c83120b
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-07-27 10:35:46 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		int num = 10, count = 1, total = 0;
+
+       while(count <= num)
+       {
+           total = total + count;
+           count++;
+       }
+
+       System.out.println("Sum of first 10 natural numbers is: "+total);
+       }
+}
