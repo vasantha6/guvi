ec24147cdb12d7e915053b5685d3de131880a031
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-07-28 19:20:13 +0530

    Update palimdrome.java

--- palimdrome.java
+++ palimdrome.java
@@ -9,10 +9,10 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int r,sum=0,temp;    
+		int r,sum=0,temp ;    
   int n=121;
   temp=n;    
-  while(n>0)
+  while (n>0)
   {    
    r=n%10;   
    sum=(sum*10)+r;    
