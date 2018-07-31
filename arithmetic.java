568277b5791f23b71a2d65b3a013623e197b7fdf
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-07-31 15:44:38 +0530

    Update arithmetic.java

--- arithmetic.java
+++ arithmetic.java
@@ -4,31 +4,27 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Arth
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-
-
-	            	Scanner input = new Scanner(System.in);
-                                    int firstTerm= 0;
-                                    int numberOfTerms = 0;
-                                    int nthTerm = 0;
-                                    int commonDifference = 0;
-                                    int  sum= 0;
-                                    int term = 0;
-                                    numberOfTerms = input.nextInt();
-                                    commonDifference = input.nextInt();
-                                    firstTerm = input.nextInt();
-                                    sum = numberOfTerms * (2 * firstTerm +(numberOfTerms - 1) * commonDifference)/2;
-                                       for(int i = 0; i < numberOfTerms;i++)
-                                        {
-                                           term = firstTerm + i * commonDifference;
-                                        }
-                                           System.out.println(sum);
-	}
-
-	
+class GFG{
+     
+   
+    static float sumOfAP(float a, float d, 
+                                  int n)
+    {
+        float sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum = sum + a;
+            a = a + d;
+        }
+        return sum;
+    }
+     
+    
+    public static void main(String args[])
+    {
+        int n = 3;
+        float a = 1, d = 1;
+        System.out.println(sumOfAP(a, d, n));
+    }
 }
+ 
