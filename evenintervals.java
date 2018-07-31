7b6394026a547fcd49753c3c0b8873690331bd7c
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-07-31 13:49:21 +0530

    Create evenintervals.java

---
+++ evenintervals.java
@@ -0,0 +1,36 @@
+	/* package whatever; // don't place package name! */
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+
+class Ideone
+
+{
+	public static void main (String[] args) throws java.lang.Exception
+	
+	{
+	
+	int p=6,q=9;
+System.out.println("Input:");
+	System.out.println(p+"  "+q);	
+		
+	for(p=p+1;p<q;p++)
+		
+	{
+				
+		if(p%2==0)
+				
+		{
+					
+			System.out.print(" "+p);
+		
+		}
+	
+}
+}
+}
