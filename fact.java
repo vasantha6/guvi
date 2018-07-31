20a1fdc7653b03a5aece118b84a848c0aa043ad7
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-07-31 14:48:56 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+		static int factorial(int n)
+        {
+         if(n==0)
+         
+          return 1;
+	     else
+          return(n*factorial(n-1));
+         }
+public static void main (String[] args)
+{
+int i,fact=1;
+int n=0;
+fact=factorial(n);
+System.out.println(fact);
+}
+}
