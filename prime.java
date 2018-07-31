1430d1c52166dff1984b338b2264db1bed32ac04
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-07-31 13:11:44 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+        int num = 29;
+        boolean flag = false;
+        for(int i = 2; i <= num/2; ++i)
+        {
+            if(num % i == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            System.out.println("yes");
+        else
+            System.out.println( "no");
+	}
+}
