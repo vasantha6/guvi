dcee85d5389c4b3c39073665e7b3fe9ad20ad5b7
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-07-31 13:30:29 +0530

    Create oddintervals.java

---
+++ oddintervals.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+
+class Ideone
+
+{
+	public static void main (String[] args) throws java.lang.Exception
+	
+	{
+	
+	int p=1,q=6;
+System.out.println("Input:");
+	System.out.println(p+"  "+q);	
+		
+	for(p=p+1;p<=q;p++)
+		
+	{
+				
+		if(p%2!=0)
+				
+		{
+					
+			System.out.print("  "+p);
+		
+		}
+	
+}
+}
+}
