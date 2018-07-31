20219b4f61602bf3df5779ef58588e8a76cdd93f
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-07-31 14:35:23 +0530

    Create armstrong.java

---
+++ armstrong.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		int c=0,a,temp;  
+		int n=371;
+		 temp=n;  
+		while(n>0)  
+		 {  
+			 a=n%10;  
+			 n=n/10;  
+			 c=c+(a*a*a);  
+		 }  
+			if(temp==c)  
+			System.out.println("yes");   
+			 else  
+			System.out.println("No");   
+	}  
+}  
+		
+
