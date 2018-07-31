1de642f4bd9b9cbd1b492cd96f8efe4ad364d275
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-07-31 13:54:22 +0530

    Update evenintervals.java

--- evenintervals.java
+++ evenintervals.java
@@ -1,32 +1,20 @@
 	/* package whatever; // don't place package name! */
 
 import java.util.*;
-
 import java.lang.*;
-
 import java.io.*;
-
 class Ideone
-
 {
 	public static void main (String[] args) throws java.lang.Exception
-	
 	{
-	
 	int p=6,q=9;
-		
-	for(p=p+1;p<q;p++)
-		
-	{
-				
-		if(p%2==0)
-				
+		for(p=p+1;p<q;p++)
 		{
-					
-			System.out.print(" "+p);
-		
+				if(p%2==0)
+		{
+				System.out.print(" "+p);
 		}
 	
-}
+		}
 }
 }
