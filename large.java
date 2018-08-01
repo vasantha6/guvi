a8146a389c99ae118c54c3f685e46bb06e76fa14
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-01 14:07:55 +0530

    Create large.java

---
+++ large.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+ class Largest_Number 
+{
+    public static void main(String[] args) 
+    {
+        int n, max;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter number of elements in the array:");
+        n = s.nextInt();
+        int a[] = new int[n];
+        System.out.println("Enter elements of array:");
+        for(int i = 0; i < n; i++)
+        {
+            a[i] = s.nextInt();
+        }
+        max = a[0];
+        for(int i = 0; i < n; i++)
+        {
+            if(max < a[i])
+            {
+                max = a[i];
+            }
+        }
+        System.out.println(+max);
+    }
+}
+
