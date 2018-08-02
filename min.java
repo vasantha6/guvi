b23a6c2aff71563cf3a579b40816c24d77b18170
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-02 14:24:19 +0530

    Update min.java

--- min.java
+++ min.java
@@ -9,27 +9,29 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
+		
 		Scanner sc= new Scanner(System.in);
-		int a[ ] = new int [10];
-		int n,i = 0;
-		n = sc.nextInt();
-		System.out.println("input:"+n);
+		int a[ ]=new int [10];
+		int n,i=0;
+		n=sc.nextInt();
+		
 		for(i=0;i<n;i++)
 		{
-			a[i] = sc.nextInt();
-			System.out.println(a[i]+" ");
+			a[i]=sc.nextInt();
+			
 			
 		}
-		int max = a[0];
+		int min=a[0];
 		for(i=0;i<n;i++)
 		{
-			if(max>a[i])
+			if(min>a[i])
 			{
-				max = a[i];
+				min=a[i];
 				
 			}
 		}
 		System.out.println();
-		System.out.println(max+" ");
+		System.out.println(min+" ");
 	}
 }
+
