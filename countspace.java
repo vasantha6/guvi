34f2adca417ce42eebdc61a8e9b6f632a2c5640a
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-10 12:23:48 +0530

    Create countspace.java

---
+++ countspace.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int count=1,i=0;
+		String s=sc.next();
+                      String arr[]=s.split("\t");
+    //OOPHelper.println("Number of spaces are: "+arr.length);
+    for(i=1;i<s.length()-1;i++)
+    {
+	    count++;	
+    }
+    System.out.println(""+count);
+	}
+}
