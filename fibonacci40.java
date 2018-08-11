461d9141adffd69aa39fa0551f2ea60a3163276a
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-11 12:38:53 +0530

    Create fibonacci40.java

---
+++ fibonacci40.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Fib
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int ch = sc.nextInt();
+		int a, b, d, n;
+		a = b = 1;
+		for (n = 1; n <= ch; n++)
+			{
+			    System.out.print(a+" ");
+			    d = a + b;
+			    a = b;
+			    b = d;
+			}
+	}
+}
