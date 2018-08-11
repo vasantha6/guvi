e2fe5708c172a5abe8ab0ddd640dc40d2973efae
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-11 10:37:10 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int k=sc.nextInt();
+		int j=sc.nextInt();
+		int i;
+		i=k;
+		k=j;
+		j=i;
+		System.out.println(k+" "+j);
+	}
+}
