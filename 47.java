e3f9cb336d61b94e315ab88a7a32b4166b5b7ebe
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-14 12:02:50 +0530

    Update 47.java

--- 47.java
+++ 47.java
@@ -4,12 +4,12 @@ class s{
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
+        int n= sc.nextInt();
         int i;
-        int a[]=new int[n];
+        int a[] =new int[n];
         for(i=0;i<n;i++)
         {
-            a[i]=sc.nextInt();
+            a[i]= sc.nextInt();
         }
         Arrays.sort(a);
         System.out.print(a[0]+" "+a[n-1]);
