29170fcd855b05b1419efa80eb0adeee1f043325
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-14 11:43:53 +0530

    Create 46.java

---
+++ 46.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int num;
+	//	int count=0;
+		
+		num=sc.nextInt();
+			num=num+1;
+		
+		System.out.println(num);
+	}
+}
