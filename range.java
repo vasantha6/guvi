e4133d419d20aec4143ad4e81e451a1b4ce5b2cc
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-14 10:32:47 +0530

    Create range.java

---
+++ range.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n>0&&n<10)
+        {
+         System.out.println("yes");
+        }
+       
+        else
+          System.out.println("no");
+	}
+}
