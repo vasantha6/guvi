6566d89deb6aa32c32d6bb959476c6b88d50bb1a
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-14 10:29:16 +0530

    Create strcat.java

---
+++ strcat.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  String s1,s2;	
+      Scanner sc=new Scanner(System.in);
+      s1=sc.next();
+      s2=sc.next();
+      System.out.println(s1+s2);
+     
+	}
+}
