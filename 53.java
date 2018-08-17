cffaccc23bdc401b51e72126e58a25ab7afac466
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-17 10:18:23 +0530

    Update 53.java

--- 53.java
+++ 53.java
@@ -3,14 +3,10 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
-		
 		Scanner sc=new Scanner(System.in);
 		int a,b,c,sum;
 		a=sc.nextInt();
