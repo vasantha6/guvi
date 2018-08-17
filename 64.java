95312c8f7a1bd5caf67e4bee421a86a25fd40059
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-17 13:33:11 +0530

    Create 64.java

---
+++ 64.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=a+b;
+        if(c%2!=0)
+        {
+         System.out.println("odd");
+         }
+        else
+         System.out.println("even");
+	}
+}
