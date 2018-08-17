638517b315feed4064dca31e250284b4460ca45a
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-17 09:44:02 +0530

    Create 41.java

---
+++ 41.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class stringks
+{
+	public static void main (String[] args) 
+	{
+		String a;
+     int k;
+     Scanner s=new Scanner(System.in);
+     k=s.nextInt();
+     a=s.next();
+     for(int i=1;i<=k;i++)
+     {
+         System.out.println(a);
+     }
+	}
+}
