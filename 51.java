059c032df5ee89f06b97bdec8980ea6e445074f6
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-17 09:55:09 +0530

    Create 51.java

---
+++ 51.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a,b,c;
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=sc.nextInt();
+		System.out.print(" "+a);
+		System.out.print(" "+b);
+		System.out.print(" "+c);
+	}
+}
