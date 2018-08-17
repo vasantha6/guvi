51f26142d8cf386f8636d5da92d46a29cdbe9f22
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-17 11:46:21 +0530

    Create 61.java

---
+++ 61.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 String d;
+     int n;
+     Scanner s=new Scanner(System.in);
+     d=s.next();
+     n=s.nextInt();
+     char a[]=d.toCharArray();
+     for(int i=0;i<n;i++)
+     {
+        System.out.printf("%c",a[i]);
+     }
+     
+
+	}
+		
+}
