efe0d704ce8632a51019cfd644d15160b9bab9c2
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-17 09:48:01 +0530

    Update 41.java

--- 41.java
+++ 41.java
@@ -3,14 +3,12 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class stringks
 {
 	public static void main (String[] args) 
 	{
 		String a;
-     int k;
+         int k;
      Scanner s=new Scanner(System.in);
      k=s.nextInt();
      a=s.next();
