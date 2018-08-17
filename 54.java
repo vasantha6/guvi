f3ad7ce18397fc3c444be55a43d0923d20f5b254
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-17 10:27:35 +0530

    Create 54.java

---
+++ 54.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n;
+		n=sc.nextInt();
+		n=n-1;
+		if((n)%2==0)
+		{
+			System.out.println(n);
+		}
+		else
+		{
+			System.out.println(n+1);
+		}
+	}
+}
