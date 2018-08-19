1883c6ef32e48b8c476973ee87894ac877349b67
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-19 07:59:55 +0530

    Create 27.java

---
+++ 27.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			Scanner sc=new Scanner(System.in);
+			char c;
+			c=sc.next().charAt(0);
+			if(c>='0'&&c<='9')
+			{
+				System.out.println("yes");
+			}
+			else
+			{
+				System.out.println("no");
+			}
+			
+	}
+}
