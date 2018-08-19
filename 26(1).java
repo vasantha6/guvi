b2d89a58c2ba37c7f33b05b9ba74a079f38d9734
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-19 07:57:49 +0530

    Update 26(1).java

--- 26(1).java
+++ 26(1).java
@@ -5,11 +5,9 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		
-        int n, temp;
+	int n, temp;
         Scanner s = new Scanner(System.in);
-        
-        n = s.nextInt();
+         n = s.nextInt();
         int a[] = new int[n];
         
         for (int i = 0; i < n; i++) 
