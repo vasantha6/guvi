1e6a6c54536b34561942e990295a48be8c649259
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-20 17:36:17 +0530

    Create 65.java

---
+++ 65.java
@@ -0,0 +1,17 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+      Scanner sc=new Scanner(System.in);
+      String s=sc.next();
+      String s2=sc.next();
+      if(System.out.printf(s+" "+s2)!=null)
+     {
+      }
+	}
+}
