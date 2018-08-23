cfc5ad3a5e971051ed990ccfa99c02c89ced0c5c
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-23 09:41:37 +0530

    Create 68.java

---
+++ 68.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a%7==0)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
