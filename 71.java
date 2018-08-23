74a8be199d0f4b2019d8dbf1fe9fdbd2fadec45f
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-23 10:16:29 +0530

    Create 71.java

---
+++ 71.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+ 
+class ChkPalindrome
+{
+   public static void main(String args[])
+   {
+      String str, rev = "";
+      Scanner sc = new Scanner(System.in);
+      str = sc.nextLine();
+      int length = str.length();
+      for ( int i=length - 1; i>=0;i-- )
+         rev = rev + str.charAt(i);
+      if (str.equals(rev))
+         System.out.println("yes");
+      else
+         System.out.println("no");
+ 
+   }
+}
