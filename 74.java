277269ef91b7fc6615e56b811b50a23c0df52724
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-23 10:51:30 +0530

    Create 74.java

---
+++ 74.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+ class Ideone1
+ {
+     public static void main(String aa[])
+     {
+        float d;
+        Scanner s=new Scanner(System.in);
+        d=s.nextFloat();
+      	System.out.print((int)Math.ceil(d));
+  
+}
+    }
