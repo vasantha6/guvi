5ffdb098196398fe9df2aebbd0ce57551b9d30e8
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-23 10:44:56 +0530

    Create 73.java

---
+++ 73.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		if(a<n&&b>n)
+		System.out.print("yes");
+		else
+		System.out.print("no");
+	}
+}
