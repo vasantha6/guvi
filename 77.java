361227ee25495639529d194e45b240f94df2c586
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-23 11:40:31 +0530

    Create 77.java

---
+++ 77.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+ 
+ class Ideone1
+ {
+	
+	public static void main(String[] args) 
+	{
+		int n, i;
+	Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		for(i=1;i<=n;i++)
+		{
+			if(n%i == 0) 
+			{
+				System.out.print(" "+i);
+			}
+		}
+	}
+}
