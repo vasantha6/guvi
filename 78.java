a36d128b565cbab2ba479d7fe14df7e555b872a7
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-23 11:45:03 +0530

    Create 78.java

---
+++ 78.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n%13==0)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
