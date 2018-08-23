a26ecdc30807867245d2447d1b9250d90a803bc6
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-23 12:06:06 +0530

    Create 79.java

---
+++ 79.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		if((n*m)/n==m&&(n*m)/n==m)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
