aa37bfeb6b023dd3d1e5d957fc93911482f504b0
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-27 10:10:11 +0530

    Create 84.java

---
+++ 84.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ class Ideone 
+{
+	public static void main(String[] args)
+  {
+     Scanner s=new Scanner(System.in);
+     char ch=s.next().charAt(0);
+     System.out.println((int)ch);
+	}
+}
