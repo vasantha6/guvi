91cdb2637ac1eadc5ad512d4baf00794eea2fdb4
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-27 09:42:38 +0530

    Create 81.java

---
+++ 81.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		int b=in.nextInt();
+		int r=Math.abs(b-a);
+		System.out.print(r);
+	}
+}
