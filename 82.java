0a4d176e51547bacde851f825fa19045d8439d3c
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-27 09:56:49 +0530

    Create 82.java

---
+++ 82.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		double a,b,c;
+		a=sc.nextDouble();
+		b=sc.nextDouble();
+		c=a*b;
+		System.out.format("%.5f",c);
+	}
+}
