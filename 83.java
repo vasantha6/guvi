75e391bff19ca7b96b2fb78bbc93f326ffd3daa0
vasantha6 <41731631+vasantha6@users.noreply.github.com>
2018-08-27 10:05:04 +0530

    Update and rename 82.java to 83.java

---
+++ 83.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=sc.nextInt();
+        int e = a / b;
+        System.out.println(e);
+        int f = c % d;
+        System.out.println(f);
+       
+}
+
+}
